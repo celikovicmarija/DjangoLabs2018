e921640d403026975157f7fa6342e313b73cf8a8
programiranje3 <devedzic@gmail.com>
2018-12-25 17:49:11 +0100

    Completed lab example

--- audiobooks/urls.py
+++ audiobooks/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 from . import views
 
+app_name = 'audiobooks'
+
 urlpatterns = [
     # audiobooks/
-    path("", views.index, name='index'),
+    # path("", views.index, name='index'),
+    path("", views.LatestReleasesView.as_view(), name = 'index'),
+
     # audiobooks/5/
-    path("<int:audiobook_id>/", views.book_detail, name='detail'),
+    # path("<int:audiobook_id>/", views.book_detail, name='detail'),
+    path("<int:pk>/", views.AudiobookDetail.as_view(), name='detail'),
+
     # audiobooks/5/review/
     path("<int:audiobook_id>/review/", views.review, name = 'review'),
+
     # audiobooks/5/after_review
-    path("<int:audiobook_id>/after_review/", views.book_detail_after_review, name = 'after_review'),
+    # path("<int:audiobook_id>/after_review/", views.book_detail_after_review, name = 'after_review'),
+    path("<int:pk>/after_review/", views.DetailAfterReview.as_view(), name = 'after_review'),
 ]
--- audiobooks/views.py
+++ audiobooks/views.py
@@ -1,22 +1,61 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Audiobook
+from django.views.generic import DetailView, ListView
 
-def index(request):
-    return HttpResponse("Hello! This is the landing page for our audiobooks app")
+class LatestReleasesView(ListView):
+    template_name = 'audiobooks/index.html'
+    context_object_name = 'recent_audiobooks'
 
-def book_detail(request, audiobook_id):
-    an_audiobook = get_object_or_404(Audiobook, id=audiobook_id)
-    context ={
-        'audiobook':an_audiobook
-    }
-    return render(request, 'audiobooks/book_detail.html', context)
+    def get_queryset(self):
+        return Audiobook.objects.order_by('-released_date')[:10]
+
+class AudiobookDetail(DetailView):
+    template_name = 'audiobooks/book_detail.html'
+    model = Audiobook
+
+class DetailAfterReview(DetailView):
+    template_name = 'audiobooks/detail_after_review.html'
+    model = Audiobook
+
+
+def handle_error(request, audiobook, err_msg):
+    return render(request, 'audiobooks/book_detail.html', {
+        'audiobook':audiobook,
+        'error_msg':err_msg
+    })
 
 def review(request, audiobook_id):
-    return HttpResponse("This view will process new review "
-                        "for audiobook with id {}".format(audiobook_id))
+    audiobook = get_object_or_404(Audiobook, id=audiobook_id)
 
-def book_detail_after_review(request, audiobook_id):
-    return HttpResponse("This page will present detailed info with the latest review "
-                        "for audiobook with id {}".format(audiobook_id))
+    try:
+        rating = request.POST['rating']
+        comment = request.POST['comment']
+    except KeyError as key_err:
+        return handle_error(request, audiobook, str(key_err))
+
+    if rating != "":
+        audiobook.review_set.create(rating=rating, comment=comment)
+        return HttpResponseRedirect( reverse('audiobooks:after_review', args=(audiobook.id,)))
+    else:
+        return handle_error(request, audiobook, "ERROR: Rating is required!")
+
+
+# def book_detail_after_review(request, audiobook_id):
+#     audiobook = get_object_or_404(Audiobook, pk=audiobook_id)
+#     return render(request, 'audiobooks/detail_after_review.html',
+#                   {'audiobook':audiobook})
+
+# def index(request):
+#     latest_audiobooks = Audiobook.objects.order_by('-released_date')[:10]
+#     context = {'recent_audiobooks':latest_audiobooks}
+#     return render(request, 'audiobooks/index.html', context)
+#
+# def book_detail(request, audiobook_id):
+#     an_audiobook = get_object_or_404(Audiobook, id=audiobook_id)
+#     context ={
+#         'audiobook':an_audiobook
+#     }
+#     return render(request, 'audiobooks/book_detail.html', context)
